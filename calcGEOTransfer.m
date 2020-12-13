cac30c318cbae1e8717c91ff3f494d5f43e3bcf2
dfuehrer <dfuehrer@email.arizona.edu>
2020-12-12 23:12:28 -0700

    edited things that dont work but i dont htink its right

--- calcGEOTransfer.m
+++ calcGEOTransfer.m
@@ -3,8 +3,9 @@ function [dv, dt, rin] = calcGEOTransfer(thetaGEO0, theta1, r1, di)
     rGEO = 42159.4856;
     disp(r1)
     omegaEarth = 7.292115856e-5;
-    % TODO make sure not negative
-    dThetaGEO = mod(theta1 - thetaGEO0, 360) * pi / 180;
+    %dThetaGEO = mod(theta1 - thetaGEO0, 360) * pi / 180;
+    dThetaGEO = (theta1 - thetaGEO0) * pi / 180;
+    disp(dThetaGEO)
     transTime = dThetaGEO / omegaEarth;
     disp(transTime);
     maxNumPhases = 6;
@@ -13,12 +14,16 @@ function [dv, dt, rin] = calcGEOTransfer(thetaGEO0, theta1, r1, di)
         asdf = @(rins) interPhaseTime(r1, rGEO, rins, k, di) - transTime;
         dt1 = asdf(r1);
         dt2 = asdf(rGEO);
-        if dt1 * dt2 > 0
-            continue
+        %if dt1 * dt2 > 0
+        %    continue
+        %end
+        %rins(k+1) = fzero(asdf, [r1, rGEO]);
+        rins(k+1) = fzero(asdf, rGEO*1.5);
+        if rins(k+1) < 6378.137
+            rins(k+1) = 0;
         end
-        rins(k+1) = fzero(asdf, [r1, rGEO]);
     end
-    rins(maxNumPhases+2) = fzero(@(rins) interPhaseTime(r1, rGEO, rins, 0, di) - transTime, rGEO*1.5);
+    %rins(maxNumPhases+2) = fzero(@(rins) interPhaseTime(r1, rGEO, rins, 0, di) - transTime, rGEO*1.5);
     %disp(interPhaseTime(r1, rGEO, rins(end), 0, di))
 
 
@@ -35,18 +40,19 @@ function [dv, dt, rin] = calcGEOTransfer(thetaGEO0, theta1, r1, di)
         dvTot(k+1) = sum(dvs(k+1, :));
         disp(dvTot(k+1));
     end
-    [time, dvs(maxNumPhases+2, :), dts(maxNumPhases+2, :)] = interPhaseTime(r1, rGEO, rins(maxNumPhases+2), 0, di);
-    disp('bielliptic')
-    dvTot(maxNumPhases+2) = sum(dvs(maxNumPhases+2, :));
-    disp(dvs(maxNumPhases+2, :))
-    disp(dvTot(maxNumPhases+2))
+    %[time, dvs(maxNumPhases+2, :), dts(maxNumPhases+2, :)] = interPhaseTime(r1, rGEO, rins(maxNumPhases+2), 0, di);
+    %disp('bielliptic')
+    %dvTot(maxNumPhases+2) = sum(dvs(maxNumPhases+2, :));
+    %disp(dvs(maxNumPhases+2, :))
+    %disp(dvTot(maxNumPhases+2))
 
     best = find(min(dvTot) == dvTot)
-    if best == maxNumPhases+2
-        kbest = 0
-    else
-        kbest = best - 1
-    end
+    %if best == maxNumPhases+2
+    %    kbest = 0
+    %else
+    %    kbest = best - 1
+    %end
+    kbest = best - 1
     dv = dvs(best, :);
     dt = dts(best, :);
     rin = rins(best);
--- interPhaseTime.m
+++ interPhaseTime.m
@@ -1,24 +1,59 @@
 %phase.m
 function [time, dv, dt] = interPhaseTime(r1, r2, rin, k, di)
     mu = 398600.442;
-    % first homann transfer to phasing orbit
-    [dv(1:2), dt(1)] = hohmann(r1, rin);
-    % phase for k periods
-    dt(2) = k .* 2*pi*sqrt(rin.^3 ./ mu);
-    %[dv(3:4), dt(3)] = hohmann(rin, r2);
-    a = (r2 + rin) / 2;     % a for transfer between phasing orbit and final
-    % find lower and outer radii for transfers since inc change at outer radius
-    rless = min([rin, r2]);
-    rmore = max([rin, r2]);
-    ind = 3 + (rin == rmore);
-    % find transfer to ro from smaller radius
-    [dv(ind), dt(3)] = halfEllipse(rless, a);
+    if k ~= 0
+        % first homann transfer to phasing orbit
+        [dv(1:2), dt(1)] = hohmann(r1, rin);
+        % phase for k periods
+        dt(2) = k .* 2*pi*sqrt(rin.^3 ./ mu);
+        %[dv(3:4), dt(3)] = hohmann(rin, r2);
+        a = (r2 + rin) / 2;     % a for transfer between phasing orbit and final
+        %% find lower and outer radii for transfers since inc change at outer radius
+        %rless = min([rin, r2]);
+        %rmore = max([rin, r2]);
+        %ind = 3 + (rin == rmore);
+        %% find transfer to ro from smaller radius
+        %[dv(ind), dt(3)] = halfEllipse(rless, a);
 
-    % find deltav to or from larger radius
-    vc = sqrt(mu ./ rmore);
-    ve = sqrt(mu .* (2 ./ rmore - 1 ./ a));
-    dv(ind - 1 + (ind == 3)*2) = sqrt(ve.^2 + vc.^2 - 2 * ve .* vc .* cos(di));
+        %% find deltav to or from larger radius
+        %vc = sqrt(mu ./ rmore);
+        %ve = sqrt(mu .* (2 ./ rmore - 1 ./ a));
+        %dv(ind - 1 + (ind == 3)*2) = sqrt(ve.^2 + vc.^2 - 2 * ve .* vc .* cos(di));
 
-    % output the total time
-    time = sum(dt);
+        % find transfer to ro from smaller radius
+        %[dv(ind), dt(3)] = halfEllipse(rin, a);
+        [dv(3), dt(3)] = halfEllipse(rin, a);
+
+        % find deltav to or from larger radius
+        vc = sqrt(mu ./ r2);
+        ve = sqrt(mu .* (2 ./ r2 - 1 ./ a));
+        %dv(ind - 1 + (ind == 3)*2) = sqrt(ve.^2 + vc.^2 - 2 * ve .* vc .* cos(di));
+        dv(4) = sqrt(ve.^2 + vc.^2 - 2 * ve .* vc .* cos(di));
+
+        % output the total time
+        time = sum(dt);
+    else
+        a1 = (r1 + rin) / 2;     % a for transfer between phasing orbit and final
+        a2 = (r2 + rin) / 2;     % a for transfer between phasing orbit and final
+        [dv(1), dt(1)] = halfEllipse(r1, a1);
+        dv(2) = 0;
+        dt(2) = 0;
+        %a = (r2 + rin) / 2;     % a for transfer between phasing orbit and final
+        %% find lower and outer radii for transfers since inc change at outer radius
+        %rless = min([rin, r2]);
+        %rmore = max([rin, r2]);
+        %ind = 3 + (rin == rmore);
+        % find transfer to ro from smaller radius
+        %[dv(ind), dt(3)] = halfEllipse(rless, a);
+        [dv(4), dt(3)] = halfEllipse(r2, a2);
+
+        % find deltav to or from larger radius
+        vin1 = sqrt(mu .* (2 ./ rin - 1 ./ a1));
+        vin2 = sqrt(mu .* (2 ./ rin - 1 ./ a2));
+        dv(3) = sqrt(vin1.^2 + vin2.^2 - 2 * vin1 .* vin2 .* cosd(di));
+        %dv(ind - 1 + (ind == 3)*2) = sqrt(vin1.^2 + vin2.^2 - 2 * vin1 .* vin2 .* cosd(di));
+
+        % output the total time
+        time = sum(dt);
+    end
 end
